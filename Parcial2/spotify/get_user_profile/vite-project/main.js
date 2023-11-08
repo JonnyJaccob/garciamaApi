d3df8b5798cfe123520cae469725ebbcc8701b8f
Josue <jaccobjosue@gmail.com>
2023-11-08 17:57:32 -0600

    Swagger 1

--- Parcial2/spotify/get_user_profile/vite-project/main.js
+++ Parcial2/spotify/get_user_profile/vite-project/main.js
@@ -29,40 +29,70 @@ const clientId = "36bbd3b49c284e93aee2c952f8e2969a";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
 
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
-} else {
-    getAccessToken(clientId, code).then(accessToken => {
-        fetchProfile(accessToken).then(profile => {
-            populateUI(profile);
-        }).catch(error => {
-            console.error('Error fetching profile:', error);
-        });
-    }).catch(error => {
-        console.error('Error getting access token:', error);
-    });
+// Ejemplo de uso de las funciones asincrónicas con async/await
+async function handleLogin() {
+  if (!code) {
+      redirectToAuthCodeFlow(clientId);
+  } else {
+      try {
+          const accessToken = await getAccessToken(clientId, code);
+          const profile = await fetchProfile(accessToken);
+          populateUI(profile);
+      } catch (error) {
+          // Manejar los errores aquí, como mostrar un mensaje al usuario
+          console.error('Error during the login process:', error);
+      }
+  }
 }
 
-function fetchProfile(token){
-    return fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    })
-    .then(response => response.json());
+// Asegúrate de que esta función también esté definida en algún lugar de tu código o importada si está en otro archivo
+async function fetchProfile(token) {
+  try {
+      const response = await fetch("https://api.spotify.com/v1/me", {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` }
+      });
+
+      const profile = await response.json();
+      return profile;
+  } catch (error) {
+      console.error('Error fetching profile:', error);
+      throw error; // Re-lanzar el error para manejarlo más adelante
+  }
 }
+
+handleLogin(); // Llama a la función que maneja el proceso de inicio de sesión
+
 
 function populateUI(profile) {
-    // Aquí asumimos que `profile` tiene la estructura que esperamos, según la interfaz UserProfile de TypeScript
-    document.getElementById("displayName").innerText = profile.display_name;
-    document.getElementById("avatar").setAttribute("src", profile.images[0].url);
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
-    // Asegúrate de que `profile.images` y `profile.images[0]` existen antes de intentar acceder a `url`
-    if (profile.images && profile.images.length > 0) {
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
-    }
+  // Establece valores predeterminados en caso de que ciertos datos no estén disponibles
+  const displayName = profile.display_name || 'Nombre no disponible';
+  const email = profile.email || 'Email no disponible';
+  const userId = profile.id || 'ID no disponible';
+  const userUri = profile.uri || '#';
+  const profileLink = profile.external_urls ? profile.external_urls.spotify : '#';
+  const profileHref = profile.href || '#';
+  
+  // Actualiza el DOM con los valores obtenidos o los valores predeterminados
+  document.getElementById("displayName").innerText = displayName;
+  document.getElementById("email").innerText = email;
+  document.getElementById("id").innerText = userId;
+  document.getElementById("uri").innerText = userUri;
+  document.getElementById("uri").setAttribute("href", profileLink);
+  document.getElementById("url").innerText = profileHref;
+  document.getElementById("url").setAttribute("href", profileHref);
+  
+  // Manejo de la imagen de perfil
+  const avatarElement = document.getElementById("avatar");
+  const imageUrlElement = document.getElementById("imgUrl");
+  if (profile.images && profile.images.length > 0) {
+      avatarElement.setAttribute("src", profile.images[0].url);
+      imageUrlElement.innerText = profile.images[0].url;
+  } else {
+      // Establece una imagen predeterminada si no hay ninguna disponible
+      const defaultAvatar = '/tron.png'; // Asegúrate de que esta ruta es correcta
+      avatarElement.setAttribute("src", defaultAvatar);
+      imageUrlElement.innerText = 'Imagen no disponible';
+  }
 }
 
